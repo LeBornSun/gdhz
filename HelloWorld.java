5383d43879cc0ccb0802ed83a715eb31ac27dc48
sun <1525353460@qq.com>
2020-07-18 19:30:37 +0800

    Commit Message

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.helloworld;
+
+/**
+ * @author Sun
+ * @date 2020-07-06 09:48
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+    }
+}
